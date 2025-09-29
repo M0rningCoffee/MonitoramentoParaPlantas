3be897a6d6af60ac7f0e2ca889b73c249799cb12
pedro.lima350 <pedro.lima350@etec.sp.gov.br>
2025-09-28 23:23:10 -0300

    update database

--- createDB_monitoramento.sql
+++ createDB_monitoramento.sql
@@ -2,11 +2,6 @@ DROP DATABASE IF EXISTS MONITORAMENTO;
 CREATE DATABASE MONITORAMENTO;
 USE MONITORAMENTO;
 
-CREATE TABLE planta(
-	id_planta INT NOT NULL PRIMARY KEY auto_increment,
-	nome_planta varchar(45) NOT null,
-	umidade int
-);
 
 CREATE TABLE solo
 (
@@ -14,11 +9,21 @@ CREATE TABLE solo
 	tipo varchar(45) NOT null
 );
 
+CREATE TABLE planta(
+	id_planta INT NOT NULL PRIMARY KEY auto_increment,
+	nome_planta varchar(45) NOT null,
+	umidade int,
+	id_solo int,
+	FOREIGN KEY (id_solo) REFERENCES solo(id_solo)
+);
+
 CREATE TABLE log
 (
 	id_log INT NOT NULL PRIMARY KEY auto_increment,
+	id_planta int,
 	estado int NOT null,
-	_data timestamp
+	_data timestamp default current_timestamp,
+	FOREIGN KEY (id_planta) REFERENCES planta(id_planta)
 );
 
 
