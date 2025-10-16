1d38c7540e1aaff97f2301a477cf29c9d7066757
Diego Gabriel Vitorino da Silva <diego.gabriel@atica.com.br>
2025-10-16 11:05:56 -0300

    Alterações para alterar login/cadastro/esqueci senha

--- app/Gardener/app/index.tsx
+++ app/Gardener/app/index.tsx
@@ -1,19 +1,10 @@
+// app/index.tsx
 import { Text, View } from "react-native";
-import Frame from "@/components/frame";
 
-export default function Index() {
-  
+export default function Home() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-      <Frame></Frame>
-
+    <View style={{flex:1,justifyContent:"center",alignItems:"center"}}>
+      <Text>HOME - App carregado com sucesso</Text>
     </View>
   );
 }
