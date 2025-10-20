44410a73ceebb663b51ba1e2ac038f7d68910073
pedro.lima350 <pedro.lima350@etec.sp.gov.br>
2025-10-19 22:48:33 -0300

    dir controllers

--- app/Gardener/app/index.tsx
+++ app/Gardener/app/index.tsx
@@ -1,10 +1,14 @@
 // app/index.tsx
-import { Text, View } from "react-native";
+import { Text, View, Button } from "react-native";
+import { useRouter } from "expo-router";
 
 export default function Home() {
+  const router = useRouter()
   return (
     <View style={{flex:1,justifyContent:"center",alignItems:"center"}}>
       <Text>HOME - App carregado com sucesso</Text>
+
+      <Button title="Ir para login" onPress={() => router.replace("/auth/login")} />
     </View>
   );
 }
